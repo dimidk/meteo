cb22051f7f1a4f57ba07c26112392a242753475a
boys <boys@boys-ThinkPad-X201.(none)>
2017-03-28 22:43:04 +0300

    port data to mysql database

--- weather.py
+++ weather.py
@@ -3,11 +3,13 @@
 
 import os
 import serial
+import sys
 import time
 import init
 from dbWeather import weather
 from sqlalchemy import func
 from sqlalchemy.orm.exc import *
+
 
 """connect to port"""
 ser=serial.Serial(init.serialPort,9600, timeout=2)
@@ -16,32 +18,51 @@ try:
 except serial.SerialException:
 	print "error:catch exception"
 	
-ser.write(':A\n')	
+"""ser.write(':A\n')"""
+ser.write(':Q\n')
 """read from port"""
-while True:
-	buf=ser.readline()
-	if buf.find('OK')!=-1:
-		continue
-	print buf
-	
-	
-	select_cmd=init.dbsession.query(weather.id,weather.info).all()
-	"""if table is empty"""
-	if select_cmd is None:
-		numrows=1
-	else:
-		"""if table is not empty get numrows"""
-		for id,info in select_cmd:
-			print id,info
-		numrows=init.dbsession.query(func.count(weather.id)).scalar()
-	print numrows
 
-	""" insert into to table"""
-	insert_file=weather(id=numrows+1,info=buf)
-	init.dbsession.add(insert_file)	
-	init.dbsession.commit()
+
+
+while True:
+	try:
+		"""buf="D,01/13,05:50:00, 75, 81,21.75,170,""" 
+		"""buf=buf+8, 78, 0.00,1650,0.266, 4.7124, 5.67,  63,!184"""
+
+
+		buf=ser.readline()
+		if buf.find('OK')!=-1:
+			continue
+		print buf
 	
-	"""init.dbsession.close()"""
+	
+		"""select_cmd=init.dbsession.query(weather.id,weather.info).all()"""
+		
+		"""if select_cmd is None
+				numrows=1
+			else:
+
+				for id,info in select_cmd:
+					print id,info
+				numrows=init.dbsession.query(func.count(weather.id)).scalar()
+			print numrows"""
+
+
+
+
+		"""insert_file=weather(info=buf)
+		init.dbsession.add(insert_file)	
+		init.dbsession.commit()	
+		time.sleep(1)"""
+	except KeyboardInterrupt:
+		print "you press Ctrl+C"
+		sys.exit()
+	
+	
+
+	
+	
+"""init.dbsession.close()"""
 	
 	
 	
