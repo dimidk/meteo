75db59ea71fcbd78bffa229cbe6034dcdaa17b90
dimitra <dimitra@epal>
2017-05-10 08:07:10 +0000

    test

--- weather.py
+++ weather.py
@@ -12,65 +12,57 @@ from sqlalchemy.orm.exc import *
 
 
 """connect to port"""
-ser=serial.Serial(init.serialPort,9600, timeout=2)
-try:
-	ser.open()
-except serial.SerialException:
-	print "error:catch exception"
-	
-"""ser.write(':A\n')"""
-ser.write(':Q\n')
-"""read from port"""
-
-
+attempt=0
+ser=serial.Serial(init.serialPort,9600,timeout=2)
+"""while True:
+	attempt+=1
+	if attempt==4:
+		print "exiting after 3  attempts to connect to port"
+		sys.exit(0)
+	print "try to connect to port"
+	try:
+		ser.open()
+		break
+	except serial.SerialException as e:
+		print e
+		print "error:catch exception"
+		print "sleeping 2 secs"
+		time.sleep(2)
+		continue"""
+ser.write(':A\n')
+"""ser.write(':Q\n')
+read from port"""
 
 while True:
 	try:
-		buf="D,01/01/17,05:50:00, 75, 81,21.75,170,"
-		buf=buf+"8, 78, 0.00,1650,0.266, 4.7124, 5.67,  63,!184"
+		"""buf="D,01/01/17,05:50:00, 75, 81,21.75,170,"
+		buf=buf+"8, 78, 0.00,1650,0.266, 4.7124, 5.67,  63,!184"""
 
 
 		buf=ser.readline()
 		if buf.startswith(' ')==True:
 			print "There must be an error"
 			exit(1)
-		elif buf.startswith('OK')==True:
+		elif buf.startswith('>')==True:
+			continue
+		elif buf=='':
 			continue
 		else:
-			print "another error"
-			exit(1)
+			print "read data"
+			
 		
 		print buf
 	
 	
-		"""select_cmd=init.dbsession.query(weather.id,weather.info).all()"""
-		
-		"""if select_cmd is None
-				numrows=1
-			else:
-
-				for id,info in select_cmd:
-					print id,info
-				numrows=init.dbsession.query(func.count(weather.id)).scalar()
-			print numrows"""
-
-
-
 
 		insert_file=weather(info=buf)
 		init.dbsession.add(insert_file)	
 		init.dbsession.commit()	
-		time.sleep(1)
+		time.sleep(20)
 	except KeyboardInterrupt:
 		print "you press Ctrl+C"
+		init.dbsession.close()
 		sys.exit()
-	
-	
-
-	
-	
-"""init.dbsession.close()"""
-	
 	
 	
 
