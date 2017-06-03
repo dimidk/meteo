af0700a58d3f907a21235c2d8a2c7df45a4db7ca
Your Name <you@example.com>
2017-06-04 00:04:17 +0300

    change to max_id

--- meteo_web.py
+++ meteo_web.py
@@ -28,7 +28,7 @@ class Index:
 		id_dict={'id':1}
 		
 		"""results=db.query("SELECT COUNT(*) AS total_info FROM weather")"""
-		results=db.query("SELECT max(id) from weather")
+		results=db.query("SELECT max(id) as total_info from weather")
 		id_dict['id']=int(results[0].total_info)
 		rec_num=id_dict['id']
 		date_str=datetime.datetime.now()
