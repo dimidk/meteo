f8cef092e2b37602742798537d2e425973e02ec8
boys <boys@boys-ThinkPad-X201.(none)>
2017-07-13 20:31:48 +0300

    ggg

--- weather.py
+++ weather.py
@@ -51,7 +51,7 @@ while True:
 	try:
 		"""buf="D,05/18,05:50:00, 75, 81,21.75,170,"
 		buf=buf+"8, 78, 0.00,1650,0.266, 4.7124, 5.67,  63,!184"""
-		"""if init.dbsession.is_active:
+	"""if init.dbsession.is_active:
 			print "session active"""
 		if exit_code==-1:
 			import init
